a896957dc7ddcfdfc054a6d892d4618aefc5d37f
tryoo127 <tryoo127@gmail.com>
2022-08-28 11:21:38 +0800

    Update recert.sh

--- recert.sh
+++ recert.sh
@@ -3,9 +3,10 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
+
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m     • RECERT / RENEW DOMAIN •       \E[0m"
+echo -e "\E[0;100;33m     • CERT / RENEW DOMAIN •       \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "" 
 echo "Please Input Your Pointing Domain In Cloudflare "
@@ -15,22 +16,23 @@ echo "$host" > /usr/local/etc/xray/domain
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo -e "" 
 #Update Sertificate SSL
-echo -e "\e[0;32mRENEW CERTIFICATE & DOMAIN IN PROGRESS\e[0m"
+echo "Automatical Update Your Sertificate SSL"
 sleep 3
 echo Starting Update SSL Sertificate
-sleep 1
+sleep 0.5
 source /var/lib/premium-script/ipvps.conf
 domain=$IP
-clear
+systemctl stop nginx
 systemctl stop xray
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
-systemctl start xray
-echo Done
-sleep 0.5
-echo -e "[${GREEN}DONE${NC}]"
-else
-echo -e "\e[1;32mPort 80 is used\e[0m"
-echo -e "\e[1;31mBefore renew domains, make sure port 80 is not used, if you are not sure whether port 80 is in use, please type info to see the active port.\e[0m"
-sleep 1
-fi
+systemctl daemon-reload
+systemctl restart nginx
+systemctl daemon-reload
+systemctl restart xray
+systemctl restart xray.service
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+m-domain
