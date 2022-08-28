f9e5a889ff564f191a91181270217fcb800d5e10
tryoo127 <tryoo127@gmail.com>
2022-08-28 11:04:11 +0800

    Add files via upload

---
+++ recert.sh
@@ -0,0 +1,43 @@
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m     • RECERT / RENEW DOMAIN •       \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "" 
+echo "Please Input Your Pointing Domain In Cloudflare "
+read -rp "Domain/Host: " -e host
+rm /etc/xray/domain
+echo "$host" > /usr/local/etc/xray/domain
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo -e "" 
+#Update Sertificate SSL
+echo "Automatical Update Your Sertificate SSL"
+sleep 3
+echo Starting Update SSL Sertificate
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+systemctl stop nginx
+systemctl stop xray
+systemctl stop xray.service
+systemctl stop trojan
+systemctl stop trojan.service
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/rare/xray/xray.crt --keypath /etc/rare/xray/xray.key --ecc
+systemctl daemon-reload
+systemctl restart nginx
+systemctl daemon-reload
+systemctl restart trojan
+systemctl restart trojan.service
+systemctl restart xray
+systemctl restart xray.service
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+m-domain
